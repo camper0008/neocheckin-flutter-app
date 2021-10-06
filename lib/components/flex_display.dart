979bca6db29055fdd2da1087e67216b3006df408
pieter <tpho.skp@edu.mercantec.dk>
2021-10-06 12:04:24 +0200

    use employee class for input

--- lib/components/flex_display.dart
+++ lib/components/flex_display.dart
@@ -1,97 +1,107 @@
+import 'dart:convert';
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:neocheckin/utils/time.dart';
+import 'package:neocheckin/models/employee.dart';
 
 class FlexDisplay extends StatefulWidget {
-  final Time flex;
-  final String name;
+  final Employee employee;
 
-  const FlexDisplay({Key? key, required this.flex, required this.name}) : super(key: key);
+  const FlexDisplay({Key? key, required this.employee}) : super(key: key);
 
   @override
   State<FlexDisplay> createState() => _FlexDisplayState();
 }
 
 class _FlexDisplayState extends State<FlexDisplay> {
-  late String _flexHours;
-  late String _flexMinutes;
   late String _flexPrefix;
   late Color _flexColor;
-  late String _name;
+  late Employee _employee;
 
-  _updateSelfState(Time flex, String name) {
-    _flexHours = flex.getFormattedHours();
-    _flexMinutes = flex.getFormattedMinutes();
+  _updateSelfState(Employee employee) {
+    _employee = employee;
 
-    bool isNegative = flex.isNegative();
+    bool isNegative = _employee.flex.isNegative();
     _flexPrefix = isNegative ? '-' : '+';
     _flexColor = isNegative ? Colors.red : Colors.green;
 
-    _name = name;
   }
 
   @override
   void initState() {
     super.initState();
-    _updateSelfState(widget.flex, widget.name);
+    _updateSelfState(widget.employee);
   }
   @override
   void didUpdateWidget(FlexDisplay oldWidget) {
     super.didUpdateWidget(oldWidget);
-    _updateSelfState(widget.flex, widget.name);
+    _updateSelfState(widget.employee);
   }
 
   @override
   Widget build(BuildContext build) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.only(right: 40), 
-          child: Image(
-            image: AssetImage('assets/images/placeholder.png')
-          ),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              _name,
+    return Visibility(
+      visible: (_employee is! NullEmployee),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 36),
+            child: Text(
+              (_employee.working ? 'Du er nu checket ud' : 'Du er nu checket ind'),
               style: const TextStyle(
-                fontSize: (14*2.75),
+                fontSize: (14*3),
               ),
-            ),
-            RichText(
-              text: TextSpan(
-                style: const TextStyle(fontSize: (14*2.25)),
-                children: <TextSpan>[
-                  const TextSpan(
-                    text: 'Flex: '
-                  ),
-                  TextSpan(
-                    text: '$_flexPrefix$_flexHours', 
-                    style: TextStyle(
-                      color: _flexColor,
-                      fontFamily: 'RobotoMono',
+            )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 40), 
+                child: Image.memory(base64Decode(_employee.photo)),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    _employee.name,
+                    style: const TextStyle(
+                      fontSize: (14*2.75),
                     ),
                   ),
-                  const TextSpan(
-                    text: ':',
-                  ),
-                  TextSpan(
-                    text: _flexMinutes, 
-                    style: TextStyle(
-                      color: _flexColor,
-                      fontFamily: 'RobotoMono',
+                  RichText(
+                    text: TextSpan(
+                      style: const TextStyle(fontSize: (14*2.25)),
+                      children: <TextSpan>[
+                        const TextSpan(
+                          text: 'Flex: '
+                        ),
+                        TextSpan(
+                          text: _flexPrefix + _employee.flex.getFormattedHours(),
+                          style: TextStyle(
+                            color: _flexColor,
+                            fontFamily: 'RobotoMono',
+                          ),
+                        ),
+                        const TextSpan(
+                          text: ':',
+                        ),
+                        TextSpan(
+                          text: _employee.flex.getFormattedMinutes(), 
+                          style: TextStyle(
+                            color: _flexColor,
+                            fontFamily: 'RobotoMono',
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
