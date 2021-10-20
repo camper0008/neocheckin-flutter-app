44b790b451a901c227656e2a0a45234b000fe100
pieter <tpho.skp@edu.mercantec.dk>
2021-10-20 16:47:18 +0200

    default values

--- lib/models/timestamp.dart
+++ lib/models/timestamp.dart
@@ -20,16 +20,17 @@ class Timestamp {
     this.dstActive = false
   });
 
+  // TODO: refactor into functions
   Timestamp.fromJson(Map<String, dynamic> json)
-    : year         = json['year'],
-      month        = json['month'],
-      day          = json['day'],
-      hour         = json['hour'],
-      minute       = json['minute'],
-      seconds      = json['seconds'],
-      milliSeconds = json['milliSeconds'],
-      isoDate          = json['dateTime'],
-      date         = json['date'],
-      time         = json['time'],
-      dstActive    = json['dstActive'];
+    : year         = json['year'].runtimeType         != null.runtimeType ? json['year'] : 0,
+      month        = json['month'].runtimeType        != null.runtimeType ? json['month'] : 0,
+      day          = json['day'].runtimeType          != null.runtimeType ? json['day'] : 0,
+      hour         = json['hour'].runtimeType         != null.runtimeType ? json['hour'] : 0,
+      minute       = json['minute'].runtimeType       != null.runtimeType ? json['minute'] : 0,
+      seconds      = json['seconds'].runtimeType      != null.runtimeType ? json['seconds'] : 0,
+      milliSeconds = json['milliSeconds'].runtimeType != null.runtimeType ? json['milliSeconds'] : 0,
+      isoDate      = json['dateTime'].runtimeType     != null.runtimeType ? json['dateTime'] : "",
+      date         = json['date'].runtimeType         != null.runtimeType ? json['date'] : "",
+      time         = json['time'].runtimeType         != null.runtimeType ? json['time'] : "",
+      dstActive    = json['dstActive'].runtimeType    != null.runtimeType ? json['dstActive'] : false;
 }
