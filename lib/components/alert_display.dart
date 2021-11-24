43c6a74f9a198f3f1339e703291ac228a330f25d
pieter <tpho.skp@edu.mercantec.dk>
2021-11-24 08:58:50 +0100

    pass context

--- lib/components/alert_display.dart
+++ lib/components/alert_display.dart
@@ -11,7 +11,7 @@ class AlertDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    playAlert();
+    playAlert(context);
     return AlertDialog(
       title: title,
       content: SingleChildScrollView(
